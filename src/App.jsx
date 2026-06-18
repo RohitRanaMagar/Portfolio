185368029054553948ce624cfb083d80e91c93f8
rowhit-mgr <mgrrohit21@icloud.com>
2026-06-18 13:18:53 +0545

    Center Academic header, add full responsiveness (1024/768/400px)

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,14 @@ import About from "./Components/pages/About";
 import Contact from "./Components/pages/Contact";
 import Skills from "./Components/pages/Skills";
 import Project from "./Components/pages/project";
+import Academic from "./Components/pages/Academic"
 function App(){
   return (
     <div>
       <Navbar/>
       <Hero/>
       <About/>
+      <Academic/>
       <Skills/>
       <Project/>
       <Contact/>
